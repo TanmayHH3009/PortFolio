e03bb569a73b630823bb7296a10855a1b85e313f
Tanmay Hatwar <tanmay.hatwar2004@gmail.com>
2024-07-03 00:40:20 +0530

    second commit

--- main.js
+++ main.js
@@ -26,3 +26,26 @@ document.addEventListener('scroll', () => {
   }
   currPos = window.scrollY;
 });
+document.getElementById("Icon").addEventListener('click', () => {
+  const classMap = {
+      'bgl50': 'bbl50',
+      'bgl300': 'bbl300',
+      'sg600': 'sb600',
+      'hsg500': 'hsb500',
+      'tp500': 'tb500',
+      'tf500': 'tf500',
+      'bgl500': 'bbl500',
+      'tl500': 'tbl500',
+      'tg600': 'tb600'
+  };
+
+  Object.keys(classMap).forEach(originalClass => {
+      document.querySelectorAll(`.${originalClass}, .${classMap[originalClass]}`).forEach(element => {
+          if (element.classList.contains(originalClass)) {
+              element.classList.replace(originalClass, classMap[originalClass]);
+          } else if (element.classList.contains(classMap[originalClass])) {
+              element.classList.replace(classMap[originalClass], originalClass);
+          }
+      });
+  });
+});
